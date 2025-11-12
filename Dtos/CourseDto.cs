daf5f625f2092e8fbda27099ccc1a58bd000d335
MRtim <timatosalikaze@gmail.com>
2025-11-12 19:40:11 +0500

    чтото было неправильно исправил уже не помню что

---
+++ Dtos/CourseDto.cs
@@ -0,0 +1,14 @@
+﻿using TestingPlatform.Domain.Enums;
+
+namespace TestingPlatform.Application.Dtos;
+
+public class CourseDto
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public TestType Type { get; set; }
+}
+
+
+
+
---
+++ Dtos/TestDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestingPlatform.Domain.Enums;
+
+namespace TestingPlatform.Application.Dtos;
+
+public class TestDto
+{
+    public int Id { get; set; }
+    public required string Title { get; set; }
+    public required string Description { get; set; }
+    public bool IsRepeatable { get; set; } = false;
+    public TestType Type { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public required DateTime PublishedAt { get; set; }
+    public required DateTime Deadline { get; set; }
+    public int DurationMinutes { get; set; }
+    public bool IsPublic { get; set; } = false;
+    public int PassingScore { get; set; }
+    public int MaxAttempts { get; set; }
+}
